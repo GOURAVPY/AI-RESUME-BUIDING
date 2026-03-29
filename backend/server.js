9fd89edb8f12011cfcf7c633c18b3d9772fce2be
GOURAVPY <gouravsuman2006@gmail.com>
2026-03-29 23:30:26 +0530

    feat: initialize backend with Express server and package.json

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// connect DB
+connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
