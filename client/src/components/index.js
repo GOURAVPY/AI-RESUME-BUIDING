99fb2d4385783112d45c777bd94a0c5d46e01b7c
GOURAVPY <gouravsuman2006@gmail.com>
2026-04-05 15:44:27 +0530

    fix: correct typo in Testimonial import path in index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,7 @@
 import Banner from "./home/Banner";
 import Hero from "./home/Hero";
 import Feature from "./home/Feature";
-import Testimonial from "./home/TEstimonial";
+import Testimonial from "./home/Testimonial";
 import CallToAction from "./home/CallToAction";
 import Footre from "./home/Footre";
 
